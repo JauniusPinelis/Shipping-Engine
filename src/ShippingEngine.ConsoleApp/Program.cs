ab59c4f25828475f35e08e0dda4e03bdf3bf259c
Jaunius Pinelis <JauniusPinelis@gmail.com>
2020-10-13 09:01:57 +0300

    Initial files + setting up dependency injection

---
+++ src/ShippingEngine.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ShippingEngine.Application;
+using System;
+
+namespace ShippingEngine
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			var services = DependencyInjection.ConfigureServices();
+
+			var serviceProvider = services.BuildServiceProvider();
+
+			serviceProvider.GetService<AppRunner>().Run();
+		}
+
+		
+}
