a148a5e94e04140d5a37e61bce73b43a2f99085d
Jaunius Pinelis <JauniusPinelis@gmail.com>
2020-10-16 08:59:20 +0300

    Refactoring to use class fixture

---
+++ tests/ShippingEngine.ApplicationTests/Fixtures/PricingServiceFixture.cs
@@ -0,0 +1,26 @@
+﻿using ShippingEngine.Application.Factories;
+using ShippingEngine.Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingEngine.ApplicationTests.Fixtures
+{
+    public class PricingServiceFixture
+    {
+		public readonly PricingService PricingService;
+		public readonly DataService DataService;
+
+		public PricingServiceFixture()
+		{
+			var _fileService = new FileService();
+			DataService = new DataService(_fileService);
+
+			var discountFactory = new DiscountFactory(DataService);
+
+			PricingService = new PricingService(DataService, discountFactory);
+		}
+	}
+}
---
+++ tests/ShippingEngine.ApplicationTests/PricingServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using ShippingEngine.Application.Factories;
+using ShippingEngine.Application.Services;
+using ShippingEngine.ApplicationTests.Fixtures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ShippingEngine.ApplicationTests
+{
+	public class PricingServiceTests : IClassFixture<PricingServiceFixture>
+	{
+		private readonly PricingService _pricingService;
+		private readonly DataService _dataService;
+
+		public PricingServiceTests(PricingServiceFixture pricingServiceFixture)
+		{
+			_pricingService = pricingServiceFixture.PricingService;
+			_dataService = pricingServiceFixture.DataService;
+		}
+
+		[Fact]
+		public void ImportDataTest()
+		{
+			_pricingService.ImportData();
+
+			var pricings = _dataService.GetPricings();
+
+			pricings.Should().NotBeEmpty();
+		}
+	}
+}
