569ab3a04d4d70fec9403eaee1fc468d5140f25b
Jaunius Pinelis <jaunius@aml-analytics.com>
2021-01-11 19:36:34 +0200

    Error

--- tests/ShippingEngine.DomainTests/DataHelperTests.cs
+++ tests/ShippingEngine.DomainTests/DataHelperTests.cs
@@ -5,47 +5,47 @@ using Xunit;
 
 namespace ShippingEngine.DomainTests
 {
-	public class DataHelperTests
-	{
-		[Theory]
-		[InlineData("2015-02-10 L LP")]
-		[InlineData("2015-02-10 S MR")]
-		public void ParseShipment_GivenCorrectFormat_ParsesShipment(string shipmentData)
-		{
-			var shipment = DataHelpers.ParseShipment(shipmentData);
+    public class DataHelperTests
+    {
+        [Theory]
+        [InlineData("2015-02-10 K LP")]
+        [InlineData("2015-02-10 S MR")]
+        public void ParseShipment_GivenCorrectFormat_ParsesShipment(string shipmentData)
+        {
+            var shipment = DataHelpers.ParseShipment(shipmentData);
 
-			shipment.Valid.Should().BeTrue();
-			shipment.ErrorMessage.Should().BeNull();
-		}
+            shipment.Valid.Should().BeTrue();
+            shipment.ErrorMessage.Should().BeNull();
+        }
 
-		[Theory]
-		[InlineData("2015-02-29 CUSPS Ignored")]
-		public void ParseShipment_GivenIncorrectFormat_ParsesShipment(string shipmentData)
-		{
-			var shipment = DataHelpers.ParseShipment(shipmentData);
+        [Theory]
+        [InlineData("2015-02-29 CUSPS Ignored")]
+        public void ParseShipment_GivenIncorrectFormat_ParsesShipment(string shipmentData)
+        {
+            var shipment = DataHelpers.ParseShipment(shipmentData);
 
-			shipment.Valid.Should().BeFalse();
-			shipment.ErrorMessage.Should().NotBeNull();
-		}
+            shipment.Valid.Should().BeFalse();
+            shipment.ErrorMessage.Should().NotBeNull();
+        }
 
-		[Theory]
-		[InlineData("LP L 6.90")]
-		[InlineData("LP L 5")]
-		public void ParsePricing_GivenCorrectFormat_ParsesPricing(string pricingData)
-		{
-			var pricing = DataHelpers.ParsePricing(pricingData);
+        [Theory]
+        [InlineData("LP L 6.90")]
+        [InlineData("LP L 5")]
+        public void ParsePricing_GivenCorrectFormat_ParsesPricing(string pricingData)
+        {
+            var pricing = DataHelpers.ParsePricing(pricingData);
 
-			pricing.Should().NotBeNull();
-		}
+            pricing.Should().NotBeNull();
+        }
 
-		[Theory]
-		[InlineData("DP L 6.90")]
-		public void ParsePricing_GiveninCorrectFormat_ParsesPricing(string pricingData)
-		{
-			Action act = () => DataHelpers.ParsePricing(pricingData);
+        [Theory]
+        [InlineData("DP L 6.90")]
+        public void ParsePricing_GiveninCorrectFormat_ParsesPricing(string pricingData)
+        {
+            Action act = () => DataHelpers.ParsePricing(pricingData);
 
-			act.Should().Throw<Exception>();
-		}
+            act.Should().Throw<Exception>();
+        }
 
-	}
+    }
 }
