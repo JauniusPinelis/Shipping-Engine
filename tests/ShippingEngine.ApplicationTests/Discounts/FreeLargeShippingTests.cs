d01379cd879ce1a76cf20e8067f818787e648cb0
Jaunius Pinelis <jaunius@aml-analytics.com>
2021-01-11 17:42:56 +0200

    Fixes

--- tests/ShippingEngine.ApplicationTests/Discounts/FreeLargeShippingTests.cs
+++ tests/ShippingEngine.ApplicationTests/Discounts/FreeLargeShippingTests.cs
@@ -1,5 +1,4 @@
 ﻿using FluentAssertions;
-using ShippingEngine.Application.Discounts;
 using ShippingEngine.ApplicationTests.Fixtures;
 using ShippingEngine.Domain.Enums;
 using ShippingEngine.Domain.Interfaces;
@@ -10,100 +9,100 @@ using Xunit;
 namespace ShippingEngine.ApplicationTests.Discounts
 {
     public class FreeLargeShippingTests : IClassFixture<DataServiceFixture>
-	{
-		private readonly IDataService _dataService;
-		public FreeLargeShippingTests(DataServiceFixture fixture)
-		{
-			_dataService = fixture.DataService;
-		}
+    {
+        private readonly IDataService _dataService;
+        public FreeLargeShippingTests(DataServiceFixture fixture)
+        {
+            _dataService = fixture.DataService;
+        }
 
-		[Theory]
-		[InlineData(Sizes.S, Providers.LP)]
-		[InlineData(Sizes.M, Providers.LP)]
-		[InlineData(Sizes.L, Providers.LP)]
-		public void CalculatePriceDiscount_NoShipments_DoesNotApplyDiscount
-			(Sizes size, Providers provider)
-		{
-			var freeLargeShippping = new FreeLargeShipping(_dataService);
+        [Theory]
+        [InlineData(Sizes.S, Providers.LP)]
+        [InlineData(Sizes.M, Providers.LP)]
+        [InlineData(Sizes.L, Providers.LP)]
+        public void CalculatePriceDiscount_NoShipments_DoesNotApplyDiscount
+            (Sizes size, Providers provider)
+        {
+            var freeLargeShippping = new FreeLargeShipping(_dataService);
 
-			var price = _dataService.GetPrice(provider, size);
+            var price = _dataService.GetPrice(provider, size);
 
-			var shipment = new Shipment()
-			{
-				Date = new DateTime(2000, 1, 1),
-				Size = size,
-				Price = price,
-				Provider = provider,
-			};
+            var shipment = new Shipment()
+            {
+                Date = new DateTime(2000, 1, 1),
+                Size = size,
+                Price = price,
+                Provider = provider,
+            };
 
-			var (discountedPrice, discount) = freeLargeShippping.CalculatePriceDiscount(shipment);
+            var (discountedPrice, discount) = freeLargeShippping.CalculatePriceDiscount(shipment);
 
-			discountedPrice.Should().HaveValue();
-			discountedPrice.Value.Should().Be(price);
+            discountedPrice.Should().HaveValue();
+            discountedPrice.Value.Should().Be(price);
 
-			discount.Should().NotHaveValue();
-		}
+            discount.Should().NotHaveValue();
+        }
 
-		[Theory]
-		[InlineData(Sizes.L, Providers.LP)]
-		public void CalculatePriceDiscount_LargeThirdTransfer_DiscountGetsApplied
-			(Sizes size, Providers provider)
-		{
-			_dataService.ClearUserInfo();
-			var date = new DateTime(2000, 1, 1);
-			var freeLargeShippping = new FreeLargeShipping(_dataService);
-			var price = _dataService.GetPrice(provider, size);
-
-
-			_dataService.IncrementLargeShipments(date);
-			_dataService.IncrementLargeShipments(date);
+        [Theory]
+        [InlineData(Sizes.L, Providers.LP)]
+        public void CalculatePriceDiscount_LargeThirdTransfer_DiscountGetsApplied
+            (Sizes size, Providers provider)
+        {
+            _dataService.ClearUserInfo();
+            var date = new DateTime(2000, 1, 1);
+            var freeLargeShippping = new FreeLargeShipping(_dataService);
+            var price = _dataService.GetPrice(provider, size);
 
 
-			var shipment = new Shipment()
-			{
-				Date = new DateTime(2000, 1, 1),
-				Size = size,
-				Price = price,
-				Provider = provider,
-			};
-
-			var (discountedPrice, discount) = freeLargeShippping.CalculatePriceDiscount(shipment);
-
-			discountedPrice.Should().HaveValue();
-			discountedPrice.Value.Should().Be(0);
-
-			discount.Should().HaveValue();
-			discount.Should().Be(price);
-		}
-
-		[Theory]
-		[InlineData(Sizes.M, Providers.LP)]
-		[InlineData(Sizes.S, Providers.LP)]
-		public void CalculatePriceDiscount_NonLargeThirdTransfer_DiscountNotApplied
-			(Sizes size, Providers provider)
-		{
-			var date = new DateTime(2000, 1, 1);
-			var freeLargeShippping = new FreeLargeShipping(_dataService);
-			var price = _dataService.GetPrice(provider, size);
-
-			_dataService.IncrementLargeShipments(date);
-			_dataService.IncrementLargeShipments(date);
+            _dataService.IncrementLargeShipments(date);
+            _dataService.IncrementLargeShipments(date);
 
 
-			var shipment = new Shipment()
-			{
-				Date = date,
-				Size = size,
-				Price = price,
-				Provider = provider,
-			};
+            var shipment = new Shipment()
+            {
+                Date = new DateTime(2000, 1, 1),
+                Size = size,
+                Price = price,
+                Provider = provider,
+            };
 
-			var (discountedPrice, discount) = freeLargeShippping.CalculatePriceDiscount(shipment);
+            var (discountedPrice, discount) = freeLargeShippping.CalculatePriceDiscount(shipment);
 
-			discountedPrice.Should().HaveValue();
-			discountedPrice.Value.Should().Be(price);
+            discountedPrice.Should().HaveValue();
+            discountedPrice.Value.Should().Be(0);
 
-			discount.Should().NotHaveValue();
-		}
-	}
+            discount.Should().HaveValue();
+            discount.Should().Be(price);
+        }
+
+        [Theory]
+        [InlineData(Sizes.M, Providers.LP)]
+        [InlineData(Sizes.S, Providers.LP)]
+        public void CalculatePriceDiscount_NonLargeThirdTransfer_DiscountNotApplied
+            (Sizes size, Providers provider)
+        {
+            var date = new DateTime(2000, 1, 1);
+            var freeLargeShippping = new FreeLargeShipping(_dataService);
+            var price = _dataService.GetPrice(provider, size);
+
+            _dataService.IncrementLargeShipments(date);
+            _dataService.IncrementLargeShipments(date);
+
+
+            var shipment = new Shipment()
+            {
+                Date = date,
+                Size = size,
+                Price = price,
+                Provider = provider,
+            };
+
+            var (discountedPrice, discount) = freeLargeShippping.CalculatePriceDiscount(shipment);
+
+            discountedPrice.Should().HaveValue();
+            discountedPrice.Value.Should().Be(price);
+
+            discount.Should().NotHaveValue();
+        }
+    }
 }
--- tests/ShippingEngine.ApplicationTests/Discounts/SmallSizeDiscountTests.cs
+++ tests/ShippingEngine.ApplicationTests/Discounts/SmallSizeDiscountTests.cs
@@ -1,5 +1,4 @@
 ﻿using FluentAssertions;
-using ShippingEngine.Application.Discounts;
 using ShippingEngine.ApplicationTests.Fixtures;
 using ShippingEngine.Domain.Enums;
 using ShippingEngine.Domain.Interfaces;
@@ -10,37 +9,37 @@ using Xunit;
 namespace ShippingEngine.ApplicationTests.Discounts
 {
     public class SmallSizeDiscountTests : IClassFixture<DataServiceFixture>
-	{
-		private readonly IDataService _dataService;
-		public SmallSizeDiscountTests(DataServiceFixture fixture)
-		{
-			_dataService = fixture.DataService;
-		}
+    {
+        private readonly IDataService _dataService;
+        public SmallSizeDiscountTests(DataServiceFixture fixture)
+        {
+            _dataService = fixture.DataService;
+        }
 
-		[Fact]
-		public void CalculatePriceDiscount_GivenLargeSize_DoesNotApplyDiscount()
-		{
-			var size = Sizes.L;
-			var provider = Providers.LP;
-			var SmallSizeDiscount = new SmallSizeDiscount(_dataService);
+        [Fact]
+        public void CalculatePriceDiscount_GivenLargeSize_DoesNotApplyDiscount()
+        {
+            var size = Sizes.L;
+            var provider = Providers.LP;
+            var SmallSizeDiscount = new SmallSizeDiscount(_dataService);
 
-			var price = _dataService.GetPrice(provider, size);
+            var price = _dataService.GetPrice(provider, size);
 
-			var shipment = new Shipment()
-			{
-				Date = new DateTime(2000, 1, 1),
-				Size = size,
-				Provider = provider,
-				Price = price
-			};
+            var shipment = new Shipment()
+            {
+                Date = new DateTime(2000, 1, 1),
+                Size = size,
+                Provider = provider,
+                Price = price
+            };
 
-			var (discountPrice, discount) = SmallSizeDiscount.CalculatePriceDiscount(shipment);
+            var (discountPrice, discount) = SmallSizeDiscount.CalculatePriceDiscount(shipment);
 
-			discountPrice.Should().HaveValue();
-			discountPrice.Value.Should().Be(price);
+            discountPrice.Should().HaveValue();
+            discountPrice.Value.Should().Be(price);
 
-			discount.Should().NotHaveValue();
-		}
-	}
+            discount.Should().NotHaveValue();
+        }
+    }
 }
 
